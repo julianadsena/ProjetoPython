ce1cecee80c6b0f4580492d0c50dc1b040140dcb
julianadsena <104790519+julianadsena@users.noreply.github.com>
2023-01-17 15:49:08 -0300

    Add files via upload

---
+++ Projeto de MadLibs em Python.py
@@ -0,0 +1,14 @@
+#Projeto de MadLibs em Python
+# Como colocar strings juntas
+#criar string que diz "se inscreva no "
+# youtuber = ""
+# #algumas maneiras de fazer isso
+# print("subscribe to" + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+adj = input ("Adjective:")
+verb1 = input("Verb:")
+verb2 = input("Verb:")
+famous_person = input("Famous person:")
+madlib = f"Computer programming is so {adj}.It makes me so excited all the time because\n I love to {verb1}.Stay hydrated and {verb2} like you are {famous_person}"
+print(madlib)
